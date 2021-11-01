dbb58ecc9184984a43afda6953c9ac741ccff41a
Robert Davis <robdave22@yahoo.com>
2021-11-01 15:15:50 -0400

    Problem 9 works

--- function-drills/functionDrillsLab.js
+++ function-drills/functionDrillsLab.js
@@ -176,7 +176,25 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 */
 
 //CODE HERE
+function bigOrSmall(arr)
+{
+  let answers = [];
 
+  for (let i = 0; i < bigOrSmallArray.length; i++)
+  {
+    if (bigOrSmallArray[i] > 100)
+    {
+      answers.push("big");
+    }
+    else if (bigOrSmallArray[i] <= 100)
+    {
+      answers.push("small");
+    }
+  }
+  return answers;
+}
+let arrayEvaluator = bigOrSmall(bigOrSmallArray);
+console.log(arrayEvaluator);
 
 ////////////////// PROBLEM 10 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
